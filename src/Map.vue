f36d94757b44cfc1d53cda8112c5324b48da361e
Adam Gaskins <adam@theadamgaskins.com>
2026-04-04 16:34:16 +0900

    Work on train a bit

--- src/Map.vue
+++ src/Map.vue
@@ -20,17 +20,70 @@ defineProps<{
 
                 <!-- train -->
                 <div
-                    class="map-icon size-9 transition-[offset-distance]"
+                    class="map-icon size-9 transition-[offset-distance] duration-[1500ms]"
                     :style="{ '--p': currentStation / stations.length }"
                 >
                     <span class="text-4xl absolute -top-2.5"> 🚃 </span>
                 </div>
+
+                <!-- <div -->
+                <!--     class="map-icon size-9 transition-[offset-distance] duration-[1500ms]" -->
+                <!--     :style="{ '--p': currentStation / stations.length, 'offset-rotate': 'auto' }" -->
+                <!-- > -->
+                <!--     <div class="train-parent size-9"> -->
+                <!--         <div class="train-model"> -->
+                <!--             <div></div> -->
+                <!--         </div> -->
+                <!--     </div> -->
+                <!-- </div> -->
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.train-parent {
+    transform-style: preserve-3d;
+    perspective: 1000px;
+    perspective-origin: center center;
+    offset-rotate: auto !important;
+}
+
+.train-model,
+.train-model::before,
+.train-model::after,
+.train-model div,
+.train-model div::before,
+.train-model div::after {
+    background-color: grey;
+    width: 100%;
+    height: 100%;
+}
+
+.train-model {
+    transform-origin: center center;
+}
+.train-model::before {
+    transform-origin: top center;
+    rotate: x 90deg;
+}
+.train-model::after {
+    transform-origin: center left;
+    rotate: y -90deg;
+}
+.train-model::after {
+    transform-origin: center left;
+    rotate: y -90deg;
+}
+.train-model div::before {
+    transform-origin: bottom center;
+    rotate: x 90deg;
+}
+.train-model div::after {
+    transform-origin: left center;
+    rotate: y 90deg;
+}
+
 .map-icon {
     display: inline-block;
     offset-path: rect(0% 100% 100% 0 round 50%);
